cb654e3eb7dfecc89b3de5b6fb7735092866bd6b
lrth06 <lrth06@gmail.com>
2022-03-29 02:49:49 +0000

    fix logging

--- dist/middleware/asyncLogger.js
+++ dist/middleware/asyncLogger.js
@@ -1,33 +1,33 @@
 import { Logging } from '@google-cloud/logging';
+const logging = new Logging();
 export default async function asyncLogger(req, res, next) {
-    function severity() {
-        if (res.statusCode === 200) {
-            return 'INFO';
-        }
-        if (res.statusCode === 404) {
-            return 'WARNING';
-        }
-        if (res.statusCode <= 500) {
-            return 'ERROR';
-        }
-        return 'INFO';
+    const log = logging.log('view-counter-log');
+    const dataObject = {
+        method: req.method,
+        url: req.url,
+        status: res.statusCode,
+        headers: { ...req.headers },
+        body: { ...req.body },
+        query: { ...req.query },
+        ip: req.ip,
+        userAgent: req.get('User-Agent'),
+        timestamp: new Date().toISOString(),
+    };
+    const metadata = {
+        resource: { type: 'global' },
+        severity: 'INFO',
+    };
+    const entry = log.entry(metadata, dataObject);
+    async function writeLog() {
+        await log.write(entry);
     }
-    if (process.env.NODE_ENV == 'production') {
-        const logging = new Logging();
-        const log = logging.log('view-counter-access');
-        const metadata = {
-            resource: { type: 'global' },
-            severity: severity(),
-        };
-        const entry = log.entry(metadata, req);
-        try {
-            await log.write(entry);
-            next();
-        }
-        catch (err) {
-            console.log(err);
-            next();
-        }
+    try {
+        await writeLog();
+        next();
+    }
+    catch (e) {
+        process.stderr.write('Error writing to log ❌\n');
+        next();
     }
 }
 //# sourceMappingURL=asyncLogger.js.map
